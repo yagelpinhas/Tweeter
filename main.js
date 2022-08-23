10a8a170d12223d5fe7b2a472e7ee0ae75b3ce6f
yagelpinhas <yagel250@gmail.com>
2022-08-23 11:59:33 +0300

    rendering

--- main.js
+++ main.js
@@ -1,91 +1,17 @@
-const Tweeter = function () {
-    let nextAvailablePostId=3
-    let nextAvailableCommentId=7
-    _posts = [
-        {
-            text: "First post!",
-            id: "p1",
-            comments: [
-                { id: "c1", text: "First comment on first post!" },
-                { id: "c2", text: "Second comment on first post!!" },
-                { id: "c3", text: "Third comment on first post!!!" }
-            ]
-        },
-        {
-            text: "Aw man, I wanted to be first",
-            id: "p2",
-            comments: [
-                { id: "c4", text: "Don't wory second poster, you'll be first one day." },
-                { id: "c5", text: "Yeah, believe in yourself!" },
-                { id: "c6", text: "Haha second place what a joke." }
-            ]
-        }
-    ]
+const tweeter = Tweeter()
+const renderer = Renderer()
 
-    const getPosts = function(){
-        return _posts
-    }
+renderer.renderPosts(tweeter.getPosts())
 
-    const addPost = function(text){
-        let post = {}
-        post.text=text
-        post.id = "p"+nextAvailablePostId
-        nextAvailablePostId+=1
-        post.comments=[]
-        _posts.push(post)
-    }
 
-    const removePost = function(id){
-        let indexToRemove=-1
-        for(let index=0;index<_posts.length;index++){
-            if(_posts[index]['id']===id){
-                indexToRemove=index
-            }
-        }
-        if(indexToRemove!=-1){ //found the post to delete
-            _posts.splice(indexToRemove,1)
-        }
-    }
 
-    const addComment = function(postID,commentText){
-        let comment={}
-        comment.id="c"+nextAvailableCommentId
-        nextAvailablePostId+=1
-        comment.text=commentText
-        for(let index=0;index<_posts.length;index++){
-            if(_posts[index]['id']===postID){
-                _posts[index]['comments'].push(comment)
-            }
-        }
-    }
 
-    const removeComment = function(postID,commentID){
-        let relevantPost;
-        for(let indexPost=0;indexPost<_posts.length;indexPost++){
-            if(_posts[indexPost]['id']===postID){
-              relevantPost=_posts[indexPost]
-            }
-        }
-        for(let indexComment=0;indexComment<relevantPost['comments'].length;indexComment++){
-            if(relevantPost['comments'][indexComment]['id']===commentID){
-                relevantPost['comments'].splice(indexComment,1)
-            }
-        }
-    }
-
-    return {
-        getPosts: getPosts,
-        addPost: addPost,
-        removePost: removePost,
-        addComment: addComment,
-        removeComment: removeComment
-    }
-}
-
-const tweeter=Tweeter()
 tweeter.addPost("Third Tweet")
-updatesPosts=tweeter.getPosts
-console.log(updatesPosts)
-tweeter.removePost("p2")
+//updatesPosts=tweeter.getPosts
+//console.log(updatesPosts)
+//tweeter.removePost("p2")
+//tweeter.addComment("p3","Seventh comment")
+//tweeter.removeComment("p1","c3")
 tweeter.addComment("p3","Seventh comment")
-tweeter.removeComment("p1","c3")
+tweeter.removeComment("p3","c7")
+renderer.renderPosts(tweeter.getPosts())
---
+++ rendering.js
@@ -0,0 +1,24 @@
+const Renderer = function() {
+    const renderPosts=function(posts){
+        $("#posts").empty();
+        for(let i=0;i<posts.length;i++){
+            let currentPost = posts[i]
+            let currentPostId = currentPost.id
+            let currentPostText = currentPost.text
+            let currentPostComments = currentPost.comments
+            const postDiv = $(`<div class="post" data-id="${currentPostId}"><h1 class="post-text">"${currentPostText}"</h1></div>`)       
+            $("#posts").append(postDiv)
+            for(let j=0;j<currentPostComments.length;j++){
+                let currentCommentId = currentPostComments[j].id
+                let currentCommentText = currentPostComments[j].text
+                const commentDiv = $(`<div class="comment" data-id="${currentCommentId}"><h1 class="comment-text">"${currentCommentText}"</h1></div>`)
+                postDiv.append(commentDiv)
+            }
+    
+        }
+    
+    }
+    return {
+        renderPosts: renderPosts
+    }
+}
